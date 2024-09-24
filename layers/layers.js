1ab2f92a1c5bb00ca1c8e06ebbbb9d52956fa640
CSU-ANRIM-Lab <esalas@centralstate.edu>
2024-09-24 16:50:03 -0400

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -1,5 +1,5 @@
 ol.proj.proj4.register(proj4);
-//ol.proj.get("ESRI:102003").setExtent([547448.318961, -20749.726902, 1638047.044016, 753677.771523]);
+//ol.proj.get("ESRI:102003").setExtent([661212.358503, 86774.091061, 1527163.106741, 667594.714880]);
 var wms_layers = [];
 
 
@@ -43,8 +43,8 @@ var lyr_SWOhioHUC12_2 = new ol.layer.Vector({
                 popuplayertitle: "SW Ohio HUC12",
                 interactive: true,
     title: 'SW Ohio HUC12<br />\
-    <img src="styles/legend/SWOhioHUC12_2_0.png" /> Not Modeled<br />\
-    <img src="styles/legend/SWOhioHUC12_2_1.png" /> Modeled<br />'
+    <img src="styles/legend/SWOhioHUC12_2_0.png" /> Not Modeled by ACPF<br />\
+    <img src="styles/legend/SWOhioHUC12_2_1.png" /> Modeled by ACPF<br />'
         });
 var format_GMandLMHUC8Boundary_3 = new ol.format.GeoJSON();
 var features_GMandLMHUC8Boundary_3 = format_GMandLMHUC8Boundary_3.readFeatures(json_GMandLMHUC8Boundary_3, 
@@ -58,20 +58,20 @@ var lyr_GMandLMHUC8Boundary_3 = new ol.layer.Vector({
                 source:jsonSource_GMandLMHUC8Boundary_3, 
                 style: style_GMandLMHUC8Boundary_3,
                 popuplayertitle: "GM and LM HUC 8 Boundary",
-                interactive: true,
+                interactive: false,
                 title: '<img src="styles/legend/GMandLMHUC8Boundary_3.png" /> GM and LM HUC 8 Boundary'
             });
 
 lyr_GoogleSatellite_0.setVisible(true);lyr_OhioBoundary_1.setVisible(true);lyr_SWOhioHUC12_2.setVisible(true);lyr_GMandLMHUC8Boundary_3.setVisible(true);
 var layersList = [lyr_GoogleSatellite_0,lyr_OhioBoundary_1,lyr_SWOhioHUC12_2,lyr_GMandLMHUC8Boundary_3];
 lyr_OhioBoundary_1.set('fieldAliases', {'FID': 'FID', 'Program': 'Program', 'State_Code': 'State_Code', 'State_Name': 'State_Name', 'Flowing_St': 'Flowing_St', 'FID_1': 'FID_1', });
-lyr_SWOhioHUC12_2.set('fieldAliases', {'OBJECTID_1': 'OBJECTID_1', 'OBJECTID_2': 'OBJECTID_2', 'OBJECTID': 'OBJECTID', 'STATENS': 'STATENS', 'ALAND': 'ALAND', 'AWATER': 'AWATER', 'INTPTLAT': 'INTPTLAT', 'INTPTLON': 'INTPTLON', 'FID_WBDHU1': 'FID_WBDHU1', 'TNMID': 'TNMID', 'METASOURCE': 'METASOURCE', 'LOADDATE': 'LOADDATE', 'AREAACRES': 'AREAACRES', 'AREASQKM': 'AREASQKM', 'STATES': 'STATES', 'HUC12': 'HUC12', 'HUTYPE': 'HUTYPE', 'HUMOD': 'HUMOD', 'TOHUC': 'TOHUC', 'NONCONTRIB': 'NONCONTRIB', 'NONCONTR_1': 'NONCONTR_1', 'Shape_Leng': 'Shape_Leng', 'ACPF': 'ACPF', 'Name': 'Name', 'Shape_Le_1': 'Shape_Le_1', 'Shape_Le_2': 'Shape_Le_2', 'Shape_Area': 'Shape_Area', });
+lyr_SWOhioHUC12_2.set('fieldAliases', {'OBJECTID_1': 'OBJECTID_1', 'OBJECTID_2': 'OBJECTID_2', 'OBJECTID': 'OBJECTID', 'STATENS': 'STATENS', 'ALAND': 'ALAND', 'AWATER': 'AWATER', 'INTPTLAT': 'INTPTLAT', 'INTPTLON': 'INTPTLON', 'FID_WBDHU1': 'FID_WBDHU1', 'TNMID': 'TNMID', 'METASOURCE': 'METASOURCE', 'LOADDATE': 'LOADDATE', 'AREAACRES': 'AREAACRES', 'AREASQKM': 'AREASQKM', 'STATES': 'STATES', 'HUC12': 'HUC12', 'HUTYPE': 'HUTYPE', 'HUMOD': 'HUMOD', 'TOHUC': 'TOHUC', 'Shape_Leng': 'Shape_Leng', 'ACPF': 'ACPF', 'Name': 'Name', 'Shape_Le_1': 'Shape_Le_1', 'Shape_Le_2': 'Shape_Le_2', 'Shape_Area': 'Shape_Area', 'Path': 'Path', 'Download': 'Download', });
 lyr_GMandLMHUC8Boundary_3.set('fieldAliases', {'OBJECTID': 'OBJECTID', 'TNMID': 'TNMID', 'METASOURCE': 'METASOURCE', 'SOURCEDATA': 'SOURCEDATA', 'SOURCEORIG': 'SOURCEORIG', 'SOURCEFEAT': 'SOURCEFEAT', 'LOADDATE': 'LOADDATE', 'GNIS_ID': 'GNIS_ID', 'AREAACRES': 'AREAACRES', 'AREASQKM': 'AREASQKM', 'STATES': 'STATES', 'HUC8': 'HUC8', 'NAME': 'NAME', 'Shape_Leng': 'Shape_Leng', 'Shape_Area': 'Shape_Area', });
 lyr_OhioBoundary_1.set('fieldImages', {'FID': 'Range', 'Program': 'TextEdit', 'State_Code': 'TextEdit', 'State_Name': 'TextEdit', 'Flowing_St': 'TextEdit', 'FID_1': 'Range', });
-lyr_SWOhioHUC12_2.set('fieldImages', {'OBJECTID_1': 'TextEdit', 'OBJECTID_2': 'TextEdit', 'OBJECTID': 'TextEdit', 'STATENS': 'TextEdit', 'ALAND': 'TextEdit', 'AWATER': 'TextEdit', 'INTPTLAT': 'TextEdit', 'INTPTLON': 'TextEdit', 'FID_WBDHU1': 'TextEdit', 'TNMID': 'TextEdit', 'METASOURCE': 'TextEdit', 'LOADDATE': 'DateTime', 'AREAACRES': 'TextEdit', 'AREASQKM': 'TextEdit', 'STATES': 'TextEdit', 'HUC12': 'TextEdit', 'HUTYPE': 'TextEdit', 'HUMOD': 'TextEdit', 'TOHUC': 'TextEdit', 'NONCONTRIB': 'TextEdit', 'NONCONTR_1': 'TextEdit', 'Shape_Leng': 'TextEdit', 'ACPF': 'TextEdit', 'Name': 'TextEdit', 'Shape_Le_1': 'TextEdit', 'Shape_Le_2': 'TextEdit', 'Shape_Area': 'TextEdit', });
+lyr_SWOhioHUC12_2.set('fieldImages', {'OBJECTID_1': 'TextEdit', 'OBJECTID_2': 'TextEdit', 'OBJECTID': 'TextEdit', 'STATENS': 'TextEdit', 'ALAND': 'TextEdit', 'AWATER': 'TextEdit', 'INTPTLAT': 'TextEdit', 'INTPTLON': 'TextEdit', 'FID_WBDHU1': 'TextEdit', 'TNMID': 'TextEdit', 'METASOURCE': 'TextEdit', 'LOADDATE': 'DateTime', 'AREAACRES': 'TextEdit', 'AREASQKM': 'TextEdit', 'STATES': 'TextEdit', 'HUC12': 'TextEdit', 'HUTYPE': 'TextEdit', 'HUMOD': 'TextEdit', 'TOHUC': 'TextEdit', 'Shape_Leng': 'TextEdit', 'ACPF': 'TextEdit', 'Name': 'TextEdit', 'Shape_Le_1': 'TextEdit', 'Shape_Le_2': 'TextEdit', 'Shape_Area': 'TextEdit', 'Path': 'TextEdit', 'Download': 'TextEdit', });
 lyr_GMandLMHUC8Boundary_3.set('fieldImages', {'OBJECTID': 'TextEdit', 'TNMID': 'TextEdit', 'METASOURCE': 'TextEdit', 'SOURCEDATA': 'TextEdit', 'SOURCEORIG': 'TextEdit', 'SOURCEFEAT': 'TextEdit', 'LOADDATE': 'DateTime', 'GNIS_ID': 'TextEdit', 'AREAACRES': 'TextEdit', 'AREASQKM': 'TextEdit', 'STATES': 'TextEdit', 'HUC8': 'TextEdit', 'NAME': 'TextEdit', 'Shape_Leng': 'TextEdit', 'Shape_Area': 'TextEdit', });
-lyr_OhioBoundary_1.set('fieldLabels', {'FID': 'no label', 'Program': 'no label', 'State_Code': 'no label', 'State_Name': 'no label', 'Flowing_St': 'no label', 'FID_1': 'no label', });
-lyr_SWOhioHUC12_2.set('fieldLabels', {'OBJECTID_1': 'hidden field', 'OBJECTID_2': 'hidden field', 'OBJECTID': 'hidden field', 'STATENS': 'hidden field', 'ALAND': 'hidden field', 'AWATER': 'hidden field', 'INTPTLAT': 'hidden field', 'INTPTLON': 'hidden field', 'FID_WBDHU1': 'hidden field', 'TNMID': 'hidden field', 'METASOURCE': 'hidden field', 'LOADDATE': 'hidden field', 'AREAACRES': 'hidden field', 'AREASQKM': 'hidden field', 'STATES': 'hidden field', 'HUC12': 'inline label - always visible', 'HUTYPE': 'hidden field', 'HUMOD': 'hidden field', 'TOHUC': 'inline label - always visible', 'NONCONTRIB': 'hidden field', 'NONCONTR_1': 'hidden field', 'Shape_Leng': 'hidden field', 'ACPF': 'inline label - always visible', 'Name': 'inline label - always visible', 'Shape_Le_1': 'hidden field', 'Shape_Le_2': 'hidden field', 'Shape_Area': 'hidden field', });
+lyr_OhioBoundary_1.set('fieldLabels', {'FID': 'hidden field', 'Program': 'hidden field', 'State_Code': 'hidden field', 'State_Name': 'hidden field', 'Flowing_St': 'hidden field', 'FID_1': 'hidden field', });
+lyr_SWOhioHUC12_2.set('fieldLabels', {'OBJECTID_1': 'hidden field', 'OBJECTID_2': 'hidden field', 'OBJECTID': 'hidden field', 'STATENS': 'hidden field', 'ALAND': 'hidden field', 'AWATER': 'hidden field', 'INTPTLAT': 'hidden field', 'INTPTLON': 'hidden field', 'FID_WBDHU1': 'hidden field', 'TNMID': 'hidden field', 'METASOURCE': 'hidden field', 'LOADDATE': 'hidden field', 'AREAACRES': 'hidden field', 'AREASQKM': 'hidden field', 'STATES': 'hidden field', 'HUC12': 'inline label - always visible', 'HUTYPE': 'hidden field', 'HUMOD': 'hidden field', 'TOHUC': 'inline label - always visible', 'Shape_Leng': 'hidden field', 'ACPF': 'inline label - always visible', 'Name': 'inline label - always visible', 'Shape_Le_1': 'hidden field', 'Shape_Le_2': 'hidden field', 'Shape_Area': 'hidden field', 'Path': 'hidden field', 'Download': 'inline label - always visible', });
 lyr_GMandLMHUC8Boundary_3.set('fieldLabels', {'OBJECTID': 'hidden field', 'TNMID': 'hidden field', 'METASOURCE': 'hidden field', 'SOURCEDATA': 'hidden field', 'SOURCEORIG': 'hidden field', 'SOURCEFEAT': 'hidden field', 'LOADDATE': 'hidden field', 'GNIS_ID': 'hidden field', 'AREAACRES': 'hidden field', 'AREASQKM': 'hidden field', 'STATES': 'hidden field', 'HUC8': 'inline label - always visible', 'NAME': 'inline label - always visible', 'Shape_Leng': 'hidden field', 'Shape_Area': 'hidden field', });
 lyr_GMandLMHUC8Boundary_3.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
